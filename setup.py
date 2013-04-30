181b5dc041c646f519f05493353671d912cd3275
Bernardo Heynemann <heynemann@gmail.com>
2013-04-30 16:36:36 -0300

    Release 0.4.7

--- setup.py
+++ setup.py
@@ -11,30 +11,36 @@
 from setuptools import setup
 from derpconf.version import __version__
 
-def run_setup(extension_modules = []):
-  setup(
-      name = 'derpconf',
-      version = __version__,
-      description = "derpconf abstracts loading configuration files for your app",
-      long_description = """
-derpconf abstracts loading configuration files for your app.
-""",
-      keywords = 'configuration',
-      author = 'globo.com',
-      author_email = 'timehome@corp.globo.com',
-      url = 'https://github.com/globocom/derpconf',
-      license = 'MIT',
-      classifiers = ['Development Status :: 4 - Beta',
-                     'Intended Audience :: Developers',
-                     'License :: OSI Approved :: MIT License',
-                     'Natural Language :: English',
-                     'Operating System :: MacOS',
-                     'Operating System :: POSIX :: Linux',
-                     'Programming Language :: Python :: 2.6',
-      ],
-      packages = ['derpconf'],
-      package_dir = {"derpconf": "derpconf"},
-      include_package_data = False
-  )
+
+def run_setup(extension_modules=[]):
+    setup(
+        name='derpconf',
+        version=__version__,
+        description="derpconf abstracts loading configuration files for your app",
+        long_description="""
+            derpconf abstracts loading configuration files for your app.
+        """,
+        keywords='configuration',
+        author='globo.com',
+        author_email='timehome@corp.globo.com',
+        url='https://github.com/globocom/derpconf',
+        license='MIT',
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: MIT License',
+            'Natural Language :: English',
+            'Operating System :: MacOS',
+            'Operating System :: POSIX :: Linux',
+            'Programming Language :: Python :: 2.6',
+        ],
+        packages=['derpconf'],
+        package_dir={"derpconf": "derpconf"},
+        install_requires=[
+            'six',
+        ],
+
+        include_package_data=False
+    )
 
 run_setup()
